5b3a8414abc1b89f72c5deca8ac31338ff8bb87a
MADLY-SOFT <kasmyninphilip208@gmail.com>
2025-12-25 15:15:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 
-BOT_TOKEN = 'YOUR_BOT_TOKEN'
+TOKEN = 'BOT_TOKEN'
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 @dp.message(CommandStart())
