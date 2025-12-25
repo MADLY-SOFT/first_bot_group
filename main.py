c94c2221b71d03a4b8e559e24b28c774ed5876c7
MADLY-SOFT <kasmyninphilip208@gmail.com>
2025-12-25 14:57:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,41 @@
-import asyncio
 import logging
-import sys
-from os import getenv
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
 
-from aiogram import Bot, Dispatcher, html
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
 
-# Bot token can be obtained via https://t.me/BotFather
-TOKEN = getenv("BOT_TOKEN")
+# Замените 'YOUR_BOT_TOKEN' на токен, который вы получили от BotFather
+API_TOKEN = 'YOUR_BOT_TOKEN'
 
-# All handlers should be attached to the Router (or Dispatcher)
+# Инициализация бота и диспетчера
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
 
-dp = Dispatcher()
-
-
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
+# Обработчик команды /start
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
     """
-    This handler receives messages with `/start` command
+    Этот обработчик будет вызываться при получении команды /start.
     """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+    await message.reply("Привет! Я твой простой Telegram бот. Отправь мне любое сообщение, и я его повторю.")
 
-
-@dp.message()
-async def echo_handler(message: Message) -> None:
+# Обработчик команды /text
+@dp.message_handler(commands=['text'])
+async def echo_text_command(message: types.Message):
     """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    Этот обработчик будет вызываться при получении команды /text.
     """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+    await message.reply("Ты отправил команду /text. Теперь отправь мне любое сообщение, и я его повторю.")
 
+# Обработчик для всех остальных текстовых сообщений (эхо)
+@dp.message_handler(content_types=types.ContentType.TEXT)
+async def echo_all(message: types.Message):
+    """
+    Этот обработчик будет повторять все текстовые сообщения, которые не являются командами.
+    """
+    await message.reply(message.text)
 
-async def main() -> None:
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
-    # And the run events dispatching
-    await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+if __name__ == '__main__':
+    # Запуск бота
+    executor.start_polling(dp, skip_updates=True)
